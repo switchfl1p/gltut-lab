c7fd4a1b9251d15e1ef421e08efa6ea1d5c87742
switchfl1p <switchfl1p@protonmail.com>
2025-11-01 21:11:00 +0200

    practice file done

--- Tut 06 Objects in Motion/Practice.cpp
+++ Tut 06 Objects in Motion/Practice.cpp
@@ -12,8 +12,8 @@
 
 GLuint theProgram;
 
-GLuint modelToCameraMatrix;
-GLuint cameraToClipMatrix;
+GLuint modelToCameraMatrixUnif;
+GLuint cameraToClipMatrixUnif;
 
 glm::mat4 cameraToClipMatrix(0.0f);
 
@@ -21,7 +21,7 @@ glm::mat4 cameraToClipMatrix(0.0f);
 float CalcFrustumScale(float fFovDeg){
     const float degToRad = (3.14159f * 2.0f) / 360.0f;
     float fFovRad = fFovDeg * degToRad;
-    return 1.0f / tan(fFovDeg / 2.0f);
+    return 1.0f / tan(fFovRad / 2.0f);
 }
 
 const float fFrustumScale = CalcFrustumScale(45.0f);
@@ -36,8 +36,8 @@ void InitializeProgram()
     theProgram = Framework::CreateProgram(shaderList);
 
     //get uniform locations
-    modelToCameraMatrix = glGetUniformLocation(theProgram, "modelToCameraMatrix");
-    cameraToClipMatrix = glGetUniformLocation(theProgram, "cameraToClipMatrix");
+    modelToCameraMatrixUnif = glGetUniformLocation(theProgram, "modelToCameraMatrix");
+    cameraToClipMatrixUnif = glGetUniformLocation(theProgram, "cameraToClipMatrix");
 
     //these values are positive by convention, and zNear can't be 0
     float fzNear = 1.0f; float fzFar = 45.0f;
@@ -52,6 +52,14 @@ void InitializeProgram()
     glUniformMatrix4fv(cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(cameraToClipMatrix));
     glUseProgram(0);
 }
+
+const int numberOfVertices = 8;
+
+#define GREEN_COLOR 0.0f, 1.0f, 0.0f, 1.0f
+#define BLUE_COLOR 	0.0f, 0.0f, 1.0f, 1.0f
+#define RED_COLOR 1.0f, 0.0f, 0.0f, 1.0f
+#define GREY_COLOR 0.8f, 0.8f, 0.8f, 1.0f
+#define BROWN_COLOR 0.5f, 0.5f, 0.0f, 1.0f
 
 //vertex position, color and index data
 const float vertexData[] =
@@ -91,8 +99,8 @@ const GLshort indexData[] =
 };
 
 GLuint vertexBufferObject;
-Gluint indexBufferObject;
-Gluint vao;
+GLuint indexBufferObject;
+GLuint vao;
 
 void InitializeVertexBuffer()
 {
@@ -138,13 +146,13 @@ glm:: vec3 BottomCircleOffset(float fElapsedTime)
 		sinf(fCurrTimeThroughLoop * fScale) * 5.f - 20.0f);
 }
 
-
 struct Instance
 {
     //defines a type named OffsetFunc
     //it's a function that has a float parameter and returns a glm::vec3
 	typedef glm::vec3(*OffsetFunc)(float);
-
+    
+    //Instance has one member variable
 	OffsetFunc CalcOffset;
 
 	glm::mat4 ConstructMatrix(float fElapsedTime)
@@ -156,3 +164,99 @@ struct Instance
 		return theMat;
 	}
 };
+
+//a list of instances with our 3 different offset functions
+Instance g_instanceList[] =
+{
+	{StationaryOffset},
+	{OvalOffset},
+	{BottomCircleOffset},
+};
+
+//Called after the window and OpenGL are initialized. Called exactly once, before the main loop.
+void init(){
+	InitializeProgram();
+	InitializeVertexBuffer();
+	
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	size_t colorDataOffset = sizeof(float) * 3 * numberOfVertices;
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorDataOffset);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+
+	glBindVertexArray(0);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
+	glDepthFunc(GL_LEQUAL);
+	glDepthRange(0.0f, 1.0f);
+}
+
+//Called to update the display.
+//You should call glutSwapBuffers after all of your rendering to display what you rendered.
+//If you need continuous updates of the screen, call glutPostRedisplay() at the end of the function.
+void display(){
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(theProgram);
+
+	glBindVertexArray(vao);
+
+	float fElapsedTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	for(int iLoop = 0; iLoop < ARRAY_COUNT(g_instanceList); iLoop++)
+	{
+		Instance &currInst = g_instanceList[iLoop];
+		const glm::mat4 &transformMatrix = currInst.ConstructMatrix(fElapsedTime);
+
+		glUniformMatrix4fv(modelToCameraMatrixUnif, 1, GL_FALSE, glm::value_ptr(transformMatrix));
+		glDrawElements(GL_TRIANGLES, ARRAY_COUNT(indexData), GL_UNSIGNED_SHORT, 0);
+	}
+
+	glBindVertexArray(0);
+	glUseProgram(0);
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+//Called whenever the window is resized. The new window size is given, in pixels.
+//This is an opportunity to call glViewport or glScissor to keep up with the change in size.
+void reshape (int w, int h)
+{
+	cameraToClipMatrix[0].x = fFrustumScale * (h / (float)w);
+	cameraToClipMatrix[1].y = fFrustumScale;
+
+	glUseProgram(theProgram);
+	glUniformMatrix4fv(cameraToClipMatrixUnif, 1, GL_FALSE, glm::value_ptr(cameraToClipMatrix));
+	glUseProgram(0);
+
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+}
+
+//Called whenever a key on the keyboard was pressed.
+//The key is given by the ''key'' parameter, which is in ASCII.
+//It's often a good idea to have the escape key (ASCII value 27) call glutLeaveMainLoop() to 
+//exit the program.
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		glutLeaveMainLoop();
+		return;
+	}
+}
+
+
+unsigned int defaults(unsigned int displayMode, int &width, int &height) {return displayMode;}
