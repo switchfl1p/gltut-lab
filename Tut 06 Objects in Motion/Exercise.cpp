2c2b8845cbd8af6f21776a9c9f568d5094e852e8
switchfl1p <switchfl1p@protonmail.com>
2025-11-02 23:50:38 +0200

    exercises beggining

---
+++ Tut 06 Objects in Motion/Exercise.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <math.h>
+#include <stdio.h>
+#include <glload/gl_3_3.h>
+#include <GL/freeglut.h>
+#include "../framework/framework.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define ARRAY_COUNT( array ) (sizeof( array ) / (sizeof( array[0] ) * (sizeof( array ) != sizeof(void*) || sizeof( array[0] ) <= sizeof(void*))))
+
+GLuint theProgram;
+GLuint positionAttrib;
+GLuint colorAttrib;
+
+GLuint modelToCameraMatrixUnif;
+GLuint cameraToClipMatrixUnif;
+
+glm::mat4 cameraToClipMatrix(0.0f);
+
+float CalcFrustumScale(float fFovDeg)
+{
+    const float degToRad = 3.14159f * 2.0f / 360.0f;
+    float fFovRad = fFovDeg * degToRad;
+    return 1.0f / tan(fFovRad / 2.0f);
+}
+
+const float fFrustumScale = CalcFrustumScale(45.0f);
